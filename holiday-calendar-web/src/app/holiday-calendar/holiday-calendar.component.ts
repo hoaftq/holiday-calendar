21c25d8d87396f98160e81ed3d30c79ed69a6cc4
hoaftq <hoaftq@gmail.com>
2024-04-29 23:00:56 +0800

    Adding tailwind

--- holiday-calendar-web/src/app/holiday-calendar/holiday-calendar.component.ts
+++ holiday-calendar-web/src/app/holiday-calendar/holiday-calendar.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HolidayCalendarService } from './holiday-calendar.service';
 import { CommonModule } from '@angular/common';
+import { CalendarComponent } from "../calendar/calendar.component";
 
 @Component({
   selector: 'app-holiday-calendar',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './holiday-calendar.component.html',
-  styleUrl: './holiday-calendar.component.scss'
+  styleUrl: './holiday-calendar.component.scss',
+  imports: [CommonModule, CalendarComponent]
 })
 export class HolidayCalendarComponent {
   holidayData$ = this.service.getHolidayData({ countryIsoCode: 'SG' });
