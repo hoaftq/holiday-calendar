6c2130219466985b569078537a98e56db519101d
hoaftq <hoaftq@gmail.com>
2024-05-05 08:33:27 +0800

    Adding environments for deployment

--- holiday-calendar-web/src/app/holiday-calendar/holiday-calendar.service.ts
+++ holiday-calendar-web/src/app/holiday-calendar/holiday-calendar.service.ts
@@ -2,16 +2,17 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RequestDto, ResponseDto } from '../types/api-types';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HolidayCalendarService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
   public getHolidayData(request: RequestDto): Observable<ResponseDto> {
     return this.httpClient.post<ResponseDto>(
-      'http://127.0.0.1:3000/holiday-calendar',
+      environment.apiUrl,
       request,
       {
         headers: new HttpHeaders({
